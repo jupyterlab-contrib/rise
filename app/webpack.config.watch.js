9cbf71d27a3f40a88428ff5e294e9f7c7a178820
AnotherCodeArtist <AnotherCodeArtist@users.noreply.github.com>
2023-02-16 14:10:15 +0100

    Fix for #8 (#9)
    * produce source maps for debugging
    
    * loading bug fixed
    
    * reset local changes in examples
    
    * Lint the code
    
    ---------
    
    Co-authored-by: AnotherCodeArtist <peter.salhofer@fh-joanneumat>
    Co-authored-by: Frédéric Collonval <fcollonval@gmail.com>

--- app/webpack.config.watch.js
+++ app/webpack.config.watch.js
@@ -1,17 +1,12 @@
 const base = require('./webpack.config');
-const ExtraWatchWebpackPlugin = require('extra-watch-webpack-plugin');
+//const ExtraWatchWebpackPlugin = require('extra-watch-webpack-plugin');
 
 module.exports = [
   {
     ...base[0],
     bail: false,
     watch: true,
-    plugins: [
-      ...base[0].plugins,
-      new ExtraWatchWebpackPlugin({
-        files: ['../packages/application/tsconfig.tsbuildinfo']
-      })
-    ]
+    plugins: [...base[0].plugins]
   },
   ...base.slice(1)
 ];
